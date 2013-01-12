c768c924c83df98e2fb6dd5ba3e0ea01535cca28
TJ Matteo <tjmatteo@gmail.com>
2013-01-12 16:36:07 -0700

    more files

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,45 @@
-$(document).ready(function(){
-	$("#makeitso").click(function(){
-		$.post("/dispatch",{
-					action: actions.getUserInfo,
-					data : "{email : test}"
-				},function(data){
-					var response = jQuery.parseJSON(data);
-                    console.log(data);
-					$("#result").html(response);
-				});
-	})
 
+$(document).ready(function(){
+    loadDashboard();
+    $("#login").click(function(){ login(); });
 });
+
+function login(){
+
+}
+function loadDashboard(){
+    $.post("/dispatch",{
+        action: actions.loadDashboard,
+        data : "{email : test}"
+    },function(data){
+        var response = jQuery.parseJSON(data);
+        console.log(data);
+    });
+}
+function loadStudents(){
+    $.post("/dispatch",{
+        action: actions.loadStudents,
+        data : "{email : test}"
+    },function(data){
+        var response = jQuery.parseJSON(data);
+        console.log(data);
+    });
+}
+function loadGroup(){
+    $.post("/dispatch",{
+        action: actions.loadGroup,
+        data : "{email : test}"
+    },function(data){
+        var response = jQuery.parseJSON(data);
+        console.log(data);
+    });
+}
+function saveGroup(){
+    $.post("/dispatch",{
+        action: actions.saveGroup,
+        data : "{email : test}"
+    },function(data){
+        var response = jQuery.parseJSON(data);
+        console.log(data);
+    });
+}
---
+++ web/js/newgroup.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: TJ
+ * Date: 1/12/13
+ * Time: 4:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
