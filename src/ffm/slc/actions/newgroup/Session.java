f955651e548f608b624f236ec3034d7e43e75405
TJ Matteo <tjmatteo@gmail.com>
2013-01-12 17:35:01 -0700

    finished dtos

---
+++ src/ffm/slc/actions/newgroup/Session.java
@@ -0,0 +1,13 @@
+package ffm.slc.actions.newgroup;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TJ
+ * Date: 1/12/13
+ * Time: 5:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Session {
+    private String name;
+    private String id;
+}
