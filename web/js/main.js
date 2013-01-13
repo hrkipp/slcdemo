fa7290a2d6d3e82a7e7f1150e888b8ef05682e05
TJ Matteo <tjmatteo@gmail.com>
2013-01-13 01:03:23 -0700

    stuff

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-    loadDashboard();
+    loadMockDashboard();
+    //loadDashboard();
 });
-
+function loadMockDashboard(){
+    var ret = {
+        name: "Linda Kim",
+        groups: [{
+            name: "Math Intervention 1",
+            id: "mi1",
+            sessions: [[1,2,3,4,5],[1,1,2,2,3],[4,3,5,2,5],[3,3,3,2,1]]
+        }]
+    };
+    renderDashboard(ret);
+}
 function loadDashboard(){
     console.log("loading dashboard");
     $.post("/dispatch",{
@@ -10,11 +21,13 @@ function loadDashboard(){
     },function(data){
         console.log(data);
         var response = jQuery.parseJSON(data);
+        console.log(response);
+        alert(response);
         renderDashboard(response);
     });
 }function renderDashboard(d){
     $("#username").html(d.name);
-    if(d.group == null) return;
+    if(d.groups == null) return;
     for( var i=0; i< d.groups.length; i++){
         var group = d.groups[i];
         var $groupWidget = $("<a class='group_widget'></a>").attr('href',"group.html?id="+group.id);
@@ -28,5 +41,6 @@ function loadDashboard(){
                 $col.append($cell)
             }
         }
+        $("#groups_container").append($groupWidget);
     }
 }
--- web/js/newgroup.js
+++ web/js/newgroup.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
     $("#create_cohort").click(function(){
         saveGroup();
     });
+    $("#num_weeks").change(function(){
+        console.log("change");
+        var $list = $(".learning_objectives").html("");
+        var num_weeks = $(this).val();
+        console.log(num_weeks);
+        for(var i=1; i<=num_weeks; i++){
+            var $lo = $("<div class='lo_row'><div class='lo_label'>Week "+i+"</div><input type='text'></div>")
+            $list.append($lo);
+        }
+    });
 });
 
 function loadStudents(s){
@@ -13,19 +23,32 @@ function loadStudents(s){
         data : "{session : "+s+"}"
     },function(data){
         var students = jQuery.parseJSON(data).students;
+        console.log(students);
+        var list = $("#student_list").html("");
         for(var i=0; i<students.length; i++){
-
+            var st = students[i];
+           var $student = $("<div><input type='checkbox' data-id='"+st.id+"'></div>");
+            list.append($student);
         }
     });
 }
 function saveGroup(){
+    var los = new Array();
+    $(".lo_row").find("input").each(function(){
+        los.push($(this).val());
+    });
+    var students = new Array();
+    $("#student_list").find("input:checked").each(function(){
+        students.push($(this).val());
+    });
     var data = {
         name : $("#name").val(),
         description : $("#description").val(),
         successCondition : $("#success_condition").val(),
-        students : $(""),
+        students : students,
         startDate : $("#start_date_picker").val(),
-        numOfWeeks : $("#num_weeks").val()
+        numOfWeeks : $("#num_weeks").val(),
+        learningObjectives : los
     }
         $.post("/dispatch",{
             action: actions.saveGroup,
@@ -46,6 +69,7 @@ function loadSections() {
         console.log(data);
         $("#username").html(data.username);
         var $select = $("#section_select");
+        $select.append("<option></option>");
         for(var i=0; i<data.sessions.length; i++){
             var session = data.sessions[i];
             $select.append("<option value='"+session.id+"'>"+session.name+"</option>");
